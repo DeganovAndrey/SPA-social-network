1242ba4398c533c51941482bf638a7d84861148a
DeganovAndrey <Deganovdyoosha@yandex.ru>
2024-12-25 20:08:14 +0300

    Added components - Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://i.pinimg.com/originals/ae/bf/f8/aebff820fd4da79db61299c3ac69bd9a.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://printwalls.ru/assets/cache_image/products/2309/fotooboi-priroda-lesa-kupit-0149_846x550_c8f.jpg"
+          alt="wallpaper"
+        />
+      </div>
+      <div>ava+description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
