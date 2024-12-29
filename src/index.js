5ecf5089c52eded5a88b6dd2481fdd9d4b4238df
DeganovAndrey <Deganovdyoosha@yandex.ru>
2024-12-29 21:26:10 +0300

    Create store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  addPost,
-  addMessage,
-  updateNewPostText,
-  updateNewMessageText,
-} from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
@@ -19,18 +13,18 @@ let rerenderEntireTree = (state) => {
       <React.StrictMode>
         <App
           state={state}
-          addPost={addPost}
-          addMessage={addMessage}
-          updateNewPostText={updateNewPostText}
-          updateNewMessageText={updateNewMessageText}
+          addPost={store.addPost.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          updateNewMessageText={store.updateNewMessageText.bind(store)}
         />
       </React.StrictMode>
     </BrowserRouter>
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,72 +1,71 @@
-let rerenderEntireTree = () => {
-  console.log("State changed");
-};
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, it's my first post", likesCount: 18 },
-      { id: 2, message: "Hi, how are you?", likesCount: 45 },
-    ],
-    newPostText: "I am frontend developer",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, it's my first post", likesCount: 18 },
+        { id: 2, message: "Hi, how are you?", likesCount: 45 },
+      ],
+      newPostText: "I am frontend developer",
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "Hello mister!" },
+        { id: 2, message: "You cool!" },
+        { id: 3, message: "How much this table?" },
+        { id: 4, message: "How are is your name?" },
+      ],
+      newMessageText: "HALO",
+      dialogs: [
+        { id: 1, name: "Andrey" },
+        { id: 2, name: "Pavel" },
+        { id: 3, name: "Sanches" },
+        { id: 4, name: "Tatyana" },
+        { id: 5, name: "Ivan" },
+      ],
+    },
+    sidebar: {},
   },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: "Hello mister!" },
-      { id: 2, message: "You cool!" },
-      { id: 3, message: "How much this table?" },
-      { id: 4, message: "How are is your name?" },
-    ],
-    newMessageText: "HALO",
-    dialogs: [
-      { id: 1, name: "Andrey" },
-      { id: 2, name: "Pavel" },
-      { id: 3, name: "Sanches" },
-      { id: 4, name: "Tatyana" },
-      { id: 5, name: "Ivan" },
-    ],
+  getState() {
+
+    return this._state;
   },
-  sidebar: {},
+  _callSubscriber() {
+    console.log("State changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 55,
+      message: this._state.profilePage.newPostText,
+      likesCount: 99,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 7,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messages.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newLetter) {
+    this._state.dialogsPage.newMessageText = newLetter;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
-window.state = state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 55,
-    message: state.profilePage.newPostText,
-    likesCount: 99,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = updateNewPostText("");
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 7,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messages.push(newMessage);
-  state.dialogsPage.newMessageText = updateNewMessageText("");
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newLetter) => {
-  state.dialogsPage.newMessageText = newLetter;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+window.store = store;
+export default store;
 
 // <img
 //   src="https://cool.klev.club/uploads/posts/2024-04/cool-klev-club-efvz-p-prikolnie-kartinki-koti-v-shapke-22.jpg"
